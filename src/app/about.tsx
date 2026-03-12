4a8a214d4b5fc8229ae4803ba08240909f446500
UnicornDad8 <cecibenitezca@gmail.com>
2026-03-12 13:52:31 -0300

    Add about screen

---
+++ src/app/about.tsx
@@ -0,0 +1,18 @@
+import { View, StyleSheet } from "react-native";
+import { Image } from "expo-image";
+
+export default function About() {
+  return (
+    <View style={styles.container}>
+      <Image source={{ uri: "https://images.ctfassets.net/sfnkq8lmu5d7/1wwJDuKWXF4niMBJE9gaSH/97b11bcd7d41039f3a8eb5c3350acdfd/2024-05-24_Doge_meme_death_-_Hero.jpg" }} style={{ width: 300, height: 300 }} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  }
+});
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,11 @@
 import { Text, View, StyleSheet } from "react-native";
-import { Image } from "expo-image";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text>Hello world!</Text>
-      <Image source={{ uri: "https://images.ctfassets.net/sfnkq8lmu5d7/1wwJDuKWXF4niMBJE9gaSH/97b11bcd7d41039f3a8eb5c3350acdfd/2024-05-24_Doge_meme_death_-_Hero.jpg" }} style={{ width: 300, height: 300 }} />
+      <Link href={"/about"}>Go to about screen</Link>
     </View>
   );
 }
