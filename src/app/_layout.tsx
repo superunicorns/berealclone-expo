b6cd9a3825c5f0e0f4ba7b0a3e9b0e241e05d209
UnicornDad8 <cecibenitezca@gmail.com>
2026-03-12 14:04:16 -0300

    Add screenOptions and Stack.Screen

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,8 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack screenOptions={{ headerStyle: { backgroundColor: "cornflowerblue" }, headerTintColor: "white", animation: "slide_from_right" }}>
+    <Stack.Screen name="index" options={{ title: "Home" }} />
+    <Stack.Screen name="about" options={{ title: "About" }} />
+  </Stack>;
 }
